ed7eaf1dae56df17c524412a5245384602ca5d14
Vicky De Palma <victoria.depalma@thoughtworks.com>
2022-02-22 09:27:56 +0100

    [AJ/VDP] Add product of several events

---
+++ src/main/java/oop/Chance.java
@@ -0,0 +1,7 @@
+package oop;
+
+public class Chance {
+    public double calculateSeveralChances(double event1, double event2) {
+        return event1 * event2;
+    }
+}
---
+++ src/test/java/oop/ChanceTest.java
@@ -0,0 +1,21 @@
+package oop;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class ChanceTest {
+
+    @Test
+    public void itShouldCalculateTheProductOfTwoChances() {
+        var die1 = new Die(6);
+        var die2 = new Die(6);
+
+        var result = new Chance().calculateSeveralChances(
+            die1.calculateProbabilityOfGettingASide(),
+            die2.calculateProbabilityOfGettingASide()
+        );
+
+        assertEquals(result, (double) 1 / 36);
+    }
+}
