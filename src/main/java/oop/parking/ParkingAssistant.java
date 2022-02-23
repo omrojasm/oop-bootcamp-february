bfae3df92513eacbf53c30b06fc2cfc0f74ef4dd
omrojasm <oscar.rojasmaillo@thoughtworks.com>
2022-02-23 15:09:45 +0100

    AnaJV/OscarR - Only park car if parking has 20% availability or more
    (cherry picked from commit 6c3bf5cc4f546e5665b97db1dcc5bb14eab0b3c9)

--- src/main/java/oop/parking/ParkingAssistant.java
+++ src/main/java/oop/parking/ParkingAssistant.java
@@ -5,22 +5,32 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ParkingAssistant {
 
-    private List<Integer> parkingLotsFreeSpots;
+    private final List<Integer> parkingLotsFreeSpots;
+    private final List<Integer> parkingInitialCapacity;
 
-    public ParkingAssistant(List<Integer> parkingLotsFreeSpots) {
+
+    public ParkingAssistant(List<Integer> parkingLotsFreeSpots, List<Integer> parkingInitialCapacity) {
         this.parkingLotsFreeSpots = parkingLotsFreeSpots;
+        this.parkingInitialCapacity = parkingInitialCapacity;
     }
 
     public void park(Car car) {
-        AtomicInteger parkingLot = new AtomicInteger(-1);
+        var parkingLotIndex = new AtomicInteger(-1);
         var space = parkingLotsFreeSpots.stream()
-                .peek(x -> parkingLot.getAndIncrement())
-                .filter(lot -> lot > 0)
-                .findFirst();
+            .peek(x -> parkingLotIndex.getAndIncrement())
+            .filter(availableSpotsForLot -> hasCapacity(availableSpotsForLot, parkingLotIndex))
+            .findFirst();
         if (space.isPresent()) {
             car.park();
-            parkingLotsFreeSpots.set(parkingLot.get(), space.get()-1);
+            parkingLotsFreeSpots.set(parkingLotIndex.get(), space.get() - 1);
         }
+    }
+
+    private boolean hasCapacity(Integer availableSpotsForLot, AtomicInteger parkingLotIndex) {
+        var initialLotCapacity = parkingInitialCapacity.get(parkingLotIndex.get());
+        final var availabilityPercentage = availableSpotsForLot.doubleValue() / initialLotCapacity.doubleValue();
+        return availabilityPercentage >= 0.2;
+
     }
 
     public List<Integer> getParkingLotsFreeSpots() {
--- src/test/java/oop/parking/ParkingAssistantTest.java
+++ src/test/java/oop/parking/ParkingAssistantTest.java
@@ -1,22 +1,30 @@
 package oop.parking;
 
 import org.mockito.Mockito;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.testng.Assert.assertEquals;
 
 public class ParkingAssistantTest {
 
-    private final Car myCar = Mockito.mock(Car.class);
+    private  Car myCar;
+
+    @BeforeMethod
+    public void setUp() {
+        myCar = Mockito.mock(Car.class);
+    }
 
     @Test
     public void itShouldBeAbleToParkACar() {
         final List<Integer> parkingLotsFreeSpots = Arrays.asList(1);
-        var assistant = new ParkingAssistant(parkingLotsFreeSpots);
+        final List<Integer> parkingInitialCapacity =  Arrays.asList(1);
+        var assistant = new ParkingAssistant(parkingLotsFreeSpots, parkingInitialCapacity);
 
         assistant.park(myCar);
 
@@ -26,11 +34,24 @@ public class ParkingAssistantTest {
     @Test
     public void itShouldParkACarInAParkingLot() {
         final List<Integer> parkingLotsFreeSpots =  Arrays.asList(10);
-        var assistant = new ParkingAssistant(parkingLotsFreeSpots);
+        final List<Integer> parkingInitialCapacity =  Arrays.asList(10);
+        var assistant = new ParkingAssistant(parkingLotsFreeSpots, parkingInitialCapacity);
 
         assistant.park(myCar);
 
-        assertEquals(assistant.getParkingLotsFreeSpots(), List.of(9));
+        assertEquals(assistant.getParkingLotsFreeSpots(), Arrays.asList(9));
 
+    }
+
+    @Test
+    public void itShouldNotParkACarfParkingIsLessThan80PercentFull() {
+        final List<Integer> parkingLotsFreeSpots =  Arrays.asList(1);
+        final List<Integer> parkingInitialCapacity =  Arrays.asList(10);
+
+        var assistant = new ParkingAssistant(parkingLotsFreeSpots, parkingInitialCapacity);
+
+        assistant.park(myCar);
+
+        verify(myCar, times(0)).park();
     }
 }
