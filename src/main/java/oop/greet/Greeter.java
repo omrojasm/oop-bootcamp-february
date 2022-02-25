c69f941f7ac7e4e1f83060c833d847351e7dcd09
Jredfern-gl <jenny.redfern@providers.glovoapp.com>
2022-02-25 11:20:22 +0100

    (Oscar/jen) - add time to greeter

--- src/main/java/oop/greet/Greeter.java
+++ src/main/java/oop/greet/Greeter.java
@@ -1,8 +1,21 @@
 package oop.greet;
 
+import java.time.LocalTime;
+
 public class Greeter {
+
+    private LocalTime timeOfTheDay;
+
+    public Greeter() {
+    }
+
+    public Greeter(LocalTime timeOfTheDay) {
+        this.timeOfTheDay = timeOfTheDay;
+    }
+
     public  String greet(String name) {
-        return "Hello " + formatName(name);
+        var greetingMessage = timeOfTheDay != null ? "Good morning " : "Hello ";
+        return greetingMessage + formatName(name);
     }
 
     private static String formatName(String name) {
--- src/test/java/oop/greet/GreeterTest.java
+++ src/test/java/oop/greet/GreeterTest.java
@@ -2,6 +2,8 @@ package oop.greet;
 
 import org.testng.annotations.Test;
 
+import java.time.LocalTime;
+
 import static org.testng.Assert.assertEquals;
 
 public class GreeterTest {
@@ -24,5 +26,11 @@ public class GreeterTest {
     public void itShouldCapitalizeFirstLetterOfTheName() {
         var greeter = new Greeter();
         assertEquals(greeter.greet("oscar"), "Hello Oscar");
+    }
+
+    @Test
+    public void itShouldGreetGoodMorningWithName() {
+        var greeter = new Greeter(LocalTime.now());
+        assertEquals(greeter.greet("Oscar"), "Good morning Oscar");
     }
 }
