9baebd406c1494b030d2184fab9313c12b41787e
omrojasm <oscar.rojasmaillo@thoughtworks.com>
2022-02-25 16:08:07 +0100

    EstefanieG/OscarR - Code formatting

--- src/main/java/oop/nato/Nato.java
+++ src/main/java/oop/nato/Nato.java
@@ -7,9 +7,9 @@ public class Nato {
     public static final String DELIMITER = "-";
 
     public static String encode(String plainMessage) {
-
-       return plainMessage.chars()
-            .mapToObj(charAt -> String.valueOf((char)charAt)).map(CodeWheel.ENCODE_MAP::get)
+        return plainMessage.chars()
+            .mapToObj(charAt -> String.valueOf((char) charAt))
+            .map(CodeWheel.ENCODE_MAP::get)
             .collect(Collectors.joining(DELIMITER));
     }
 
