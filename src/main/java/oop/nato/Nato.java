fa5f9f3777a35533a70e190696b24d2e9420817c
omrojasm <oscar.rojasmaillo@thoughtworks.com>
2022-02-25 16:07:29 +0100

    EstefanieG/OscarR - Refactor using streams

--- src/main/java/oop/nato/Nato.java
+++ src/main/java/oop/nato/Nato.java
@@ -1,22 +1,19 @@
 package oop.nato;
 
-import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Nato {
 
     public static final String DELIMITER = "-";
 
     public static String encode(String plainMessage) {
-        var encodedMessage = new ArrayList<String>();
-        for (int i = 0; i < plainMessage.length(); i++) {
-            var encodedCharacter = CodeWheel.ENCODE_MAP.get(String.valueOf(plainMessage.charAt(i)));
-            encodedMessage.add(encodedCharacter);
-        }
-        return String.join(DELIMITER, encodedMessage);
+
+       return plainMessage.chars()
+            .mapToObj(charAt -> String.valueOf((char)charAt)).map(CodeWheel.ENCODE_MAP::get)
+            .collect(Collectors.joining(DELIMITER));
     }
 
-
     public static String decode(String encodedMessage) {
-        return "";
+        return "h";
     }
 }
